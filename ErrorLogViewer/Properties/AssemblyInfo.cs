9c5c11a853f6f0167a17253706ba1c964491c467
bitm0de <aceofspin@hotmail.com>
2020-02-20 22:09:35 -0700

    Updated assembly info and added gitignore file.

--- ErrorLogViewer/Properties/AssemblyInfo.cs
+++ ErrorLogViewer/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("ErrorLogViewer")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ErrorLogViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyCopyright("Copyright © Troy Garner")]
+[assembly: AssemblyVersion("1.0.*")]
 
